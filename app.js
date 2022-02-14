d2a3e1286f92e92f9fdde3a0e5bc3f0480a69e9e
root <root@DESKTOP-V8A11UG.localdomain>
2022-02-14 17:08:30 +0000

    This commit contains the updated app, model, controller and test files for the article/:article_id endpoint

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
 const { getTopics, getArticle } = require('./controllers/controllers')
-
 app.use(express.json());
 
 app.get('/api/topics', getTopics);
+app.get('/api/articles/:article_id', getArticle)
 
 
 app.all('/*', (req, res) => {
     res.status(404).send({ msg: '404 - Path not found'})
 })
+
+app.use((err, req, res, next) => {
+    if(err.status && err.msg) {
+        res.status(err.status).send({ msg: err.msg })
+    } else next(err);
+});
+app.use((err, req, res, next) => {
+    if(err.code === "22P02") {
+        res.status(400).send({msg: "bad request"});
+    } else next(err);
+});
+app.use((err, req, res, next) => {
+    res.status(500).send({msg: 'Internal Server Error' })
+});
 
 module.exports = app;
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,8 +1,23 @@
 const { selectTopics, selectArticle } = require('../models/models')
 
-exports.getTopics = (req, res) => {
+exports.getTopics = (req, res, next) => {
     selectTopics().then((topics) => {
         res.status(200).send({topics})
     })
+    .catch((err) => {
+        next(err)
+    })
 }
 
+exports.getArticle = (req, res, next) => {
+    const articleID  = req.params.article_id;
+    console.log(articleID)
+    selectArticle(articleID).then((article) => {
+        console.log('Hello', '<--------------------------')
+        res.status(200).send(article);
+    })
+    .catch((error) => {
+        next(error);
+    })
+
+}
