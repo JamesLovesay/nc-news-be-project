be248ce261b1778e29e16dcc1ac0747549cd0a6e
root <root@DESKTOP-V8A11UG.localdomain>
2022-02-16 14:49:14 +0000

    This commit contains the updated test, app, controller and model files for ticket 15 get comments by article id

--- models/models.js
+++ models/models.js
@@ -35,3 +35,13 @@ exports.selectArticles = () => {
         return rows
     })
 }
+
+exports.checkArticleExists = (articleId, comments) => {
+    return db.query('SELECT * FROM articles WHERE article_id = $1;', [articleId])
+    .then(({ rows }) => {
+        if (rows.length === 0) {
+            return Promise.reject({ status: 404, msg: "404 - Path not found" })
+        }
+        return comments;
+    })
+}
