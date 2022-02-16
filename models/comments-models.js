596fac4287475b654ac86762fbc83d68d30dee36
root <root@DESKTOP-V8A11UG.localdomain>
2022-02-16 14:50:17 +0000

    This commit contains the updated comments-models file to remove the console log.

--- models/comments-models.js
+++ models/comments-models.js
@@ -4,7 +4,6 @@ const { checkArticleExists } = require('./models.js');
 exports.selectCommentsByArticleId = (id) => {
     return db.query('SELECT comment_id, votes, created_at, author, body FROM comments WHERE article_id = $1;', [id]).then(({ rows }) => {
         if (rows.length === 0) {
-            console.log(rows)
             return checkArticleExists(id, rows)
         }
         return rows;
