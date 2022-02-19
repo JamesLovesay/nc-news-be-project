c8517c32032726f105824e1d923374e04e8fcf49
root <root@DESKTOP-V8A11UG.localdomain>
2022-02-19 14:36:59 +0000

    This commit contains the updated app, test, controller and model files for the endpoint ticket 22 GET request for a user object by username, alongside an updated endpoints.json file.

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,6 +1,7 @@
-const { getUsers } = require('../controllers/user-controllers');
+const { getUsers, getUser } = require('../controllers/user-controllers');
 const usersRouter = require('express').Router();
 
 usersRouter.get('/', getUsers)
+usersRouter.get('/:username', getUser)
 
 module.exports = usersRouter;
