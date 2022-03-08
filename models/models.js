c4691240484191a4b3ca933d46284cc3415ecd05
root <root@DESKTOP-V8A11UG.localdomain>
2022-03-08 15:06:20 +0000

    refactor get articles function to dynamically check if topic is valid

--- models/models.js
+++ models/models.js
@@ -44,35 +44,56 @@ exports.amendArticleById = (articleId, changesToArticle) => {
 exports.selectArticles = (sort_by = 'created_at', order = 'DESC', topic, limit = 10, p = 1) => {
     const validSortBy = ['article_id', 'title', 'topic', 'author', 'body', 'created_at', 'votes'];
     const validOrder = ['asc', 'desc', 'ASC', 'DESC'];
-    const validTopics = ['cats', 'mitch', 'paper'];
+    if(!validSortBy.includes(sort_by) || !validOrder.includes(order) || typeof parseInt(limit) !== 'number' || typeof parseInt(p) !== 'number') {
+    return Promise.reject({status: 400, msg: 'Invalid sort query'})
+    }
 
-    if((topic && !validTopics.includes(topic)) || !validSortBy.includes(sort_by) || !validOrder.includes(order)) {
-        if(!validSortBy.includes(sort_by) || !validOrder.includes(order) || typeof limit !== 'number' || typeof p !== 'number') {
-            return Promise.reject({status: 400, msg: 'Invalid sort query'})
-        }
-        if(topic && !validTopics.includes(topic)) {
-            return Promise.reject({status: 404, msg: '404 - topic not found'})
-        }
+    if(topic) {
+    return db.query('SELECT * FROM topics WHERE slug = $1;', [topic])
+    .then(({ rows }) => {
+        if (rows.length === 0) {
+            return Promise.reject({ status: 404, msg: "404 - topic not found" })
+            } else {
+                    queryValues = [];
+    let queryStr = `SELECT articles.*, CAST(COUNT(articles.article_id) AS INT) AS total_count, CAST(COUNT(comments.comment_id) AS INT) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id `;
+    if(topic) {
+        queryStr += 'WHERE topic = $1 '
+        queryValues.push(topic)
+    }
+    queryStr += `GROUP BY articles.article_id ORDER BY ${sort_by} ${order.toUpperCase()}`
+    if(limit) {
+            queryStr += ` LIMIT ${limit} OFFSET (${p} - 1) * ${limit};`  
     } else {
-        queryValues = [];
-        let queryStr = `SELECT articles.*, CAST(COUNT(articles.article_id) AS INT) AS total_count, CAST(COUNT(comments.comment_id) AS INT) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id `;
-        if(topic) {
-            queryStr += 'WHERE topic = $1 '
-            queryValues.push(topic)
-        }
-        queryStr += `GROUP BY articles.article_id ORDER BY ${sort_by} ${order.toUpperCase()}`
-        if(limit) {
-                queryStr += ` LIMIT ${limit} OFFSET (${p} - 1) * ${limit};`  
-        } else {
-            queryStr += ";"
-        }
-        
-        return db.query(queryStr, queryValues)
-        .then(({rows}) => {
-            return rows
-            })
-        }
-}
+        queryStr += ";"
+    }
+
+    return db.query(queryStr, queryValues)
+    .then(({rows}) => {
+    return rows
+    })
+            }
+        })
+    } 
+
+    queryValues = [];
+    let queryStr = `SELECT articles.*, CAST(COUNT(articles.article_id) AS INT) AS total_count, CAST(COUNT(comments.comment_id) AS INT) AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id `;
+    if(topic) {
+        queryStr += 'WHERE topic = $1 '
+        queryValues.push(topic)
+    }
+    queryStr += `GROUP BY articles.article_id ORDER BY ${sort_by} ${order.toUpperCase()}`
+    if(limit) {
+            queryStr += ` LIMIT ${limit} OFFSET (${p} - 1) * ${limit};`  
+    } else {
+        queryStr += ";"
+    }
+
+    return db.query(queryStr, queryValues)
+    .then(({rows}) => {
+    return rows
+    })
+} 
+
 
 exports.checkArticleExists = (articleId, comments) => {
     return db.query('SELECT * FROM articles WHERE article_id = $1;', [articleId])
